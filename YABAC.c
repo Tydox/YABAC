850bd4734df9161b6baaadf794e734b7d2752c68
Tydox <daniel.mizrahi@ruppin365.net>
2020-04-08 01:23:56 +0300

    update

--- YABAC.c
+++ YABAC.c
@@ -4,16 +4,22 @@ int main(void) {
     for (int c = 0; c <= --(TT->r_size); ++c)
     {
         //get the size of the invisible cube
-        int box_size = (TT->r_size) / (2 * pow(2, c);
-
+        int box_size = (TT->r_size) / (2 * pow(2, c));
+     
+        int sum = 0;
+        int temp = 0;
         for (int r = 0; c <= --(TT->c_size); ++r)
         {
-            int sum = 0;
-            int temp = box_size;
-            if (temp == 0)
-                temp = box_size;
-            bm_write(TT, r, c, 0));
-        --temp;
+     
+            if (temp == box_size)
+            temp = 0;
+            sum = !sum;
+
+            if(temp<box_size)
+            bm_write(TT, r, c, 0);
+            
+            --temp;
+            ++sum;
 
         }
     }
