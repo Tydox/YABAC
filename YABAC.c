2c15360a4e2d6425cba20488d0caf9f2a9351ecd
Tydox <daniel.mizrahi@ruppin365.net>
2020-04-08 01:30:04 +0300

    v0.1

--- YABAC.c
+++ YABAC.c
@@ -5,21 +5,29 @@ int main(void) {
     {
         //get the size of the invisible cube
         int box_size = (TT->r_size) / (2 * pow(2, c));
-     
-        int sum = 0;
-        int temp = 0;
+        //switch will be 0 or 1
+        int switcher = 0;
+        //indicator will tell us position on the row
+        int indicator = 0;
+
         for (int r = 0; c <= --(TT->c_size); ++r)
         {
-     
-            if (temp == box_size)
-            temp = 0;
-            sum = !sum;
+            //if indicator reached last row, reset it and flip the switch
+            if (indicator == box_size)
+            indicator = 0;
+            switcher = !switcher;
 
-            if(temp<box_size)
-            bm_write(TT, r, c, 0);
+
+            if (switcher == 0) {
+                if (indicator <= box_size)
+                    bm_write(TT, r, c, 0);
+            }
+            else {
+                if (indicator <= box_size)
+                    bm_write(TT, r, c, 1);
+            }
             
-            --temp;
-            ++sum;
+            ++indicator;
 
         }
     }
