238ac783ca0b8105de2a30f6adbc923a320813f1
Tydox <daniel.mizrahi@ruppin365.net>
2020-04-18 03:46:56 +0300

    update

--- YABAC.c
+++ YABAC.c
@@ -5,63 +5,43 @@
 #include "bm.h"
 
 
-int main(void) {
+int main(void)
+{
+		BM* TT = bm_make(16, 4);
+		for (int c = 0; c <= --(TT->r_size); ++c)
+			{
+				//get the size of the invisible cube
+				int box_size = (TT->r_size) / (2 * pow(2, c)); //8
+				//input 0 or 1
+				int tick = 0;
+				//amount of switches
+				int ticksum = (TT->r_size) / box_size; //2
+				//count input rows
+				int count = 1;
+				//sum of flips of 0 and 1	
+				int flip = 0;
 
-    BM* TT = bm_make(16, 4);
-    for (int c = 0; c <= --(TT->r_size); ++c)
-    {
-        //get the size of the invisible cube
-        int box_size = (TT->r_size) / (2 * pow(2, c)); //8
-        //input 0 or 1
-        int tick = 0;
-        //amount of switches
-        int ticksum = (TT->r_size)/box_size; //2
-        //count input rows
-        int count=1;
-    	//sum of flips of 0 and 1	
-    	int flip=0;
+				if (flip < ticksum)
+					{
+						for (int r = 0; c <= --(TT->c_size); ++r)
+							{
+								if (count < box_size)
+									{
+										if (tick == 0)
+											bm_write(TT, r, c, 0);
+										else if (tick == 1)
+											bm_write(TT, r, c, 1);
 
-        if(flip<ticksum){
-        for (int r = 0; c <= --(TT->c_size); ++r)
-        {
-        if(count<box_size)
-        {
-             if(tick==0)
-            bm_write(TT,r,c,0);
-             else if(tick==1)
-			bm_write(TT,r,c,1);
+										++count;
+									}
+								else if (count == box_size)
+									{
+										++flip;
+										tick = !tick;
+									}
+							}
+					}
+			}
 
-        	++count;	
-        }
-        else if (count==box_size)
-        {
-	        ++flip;
-        	tick=!tick;
-        }
-        }
-        }
-    }
-		
 		bm_print(TT);
-
 }
-
-
-/*
-   //if indicator reached last row, reset it and flip the switch
-            if (indicator == box_size)
-            indicator = 0;
-            switcher = !switcher;
-
-
-            if (switcher == 0) {
-                if (indicator <= box_size)
-                    bm_write(TT, r, c, 0);
-                
-            }
-            else {
-                if (indicator <= box_size)
-                    bm_write(TT, r, c, 1);
-            }
-            
-*/
