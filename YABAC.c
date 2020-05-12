febfc26f9dce37c05a8eaa40965227652e0156f9
Tydox <daniel.mizrahi@ruppin365.net>
2020-05-12 18:45:28 +0300

    loop fixed

--- YABAC.c
+++ YABAC.c
@@ -7,41 +7,78 @@
 
 int main(void)
 {
-		BM* TT = bm_make(16, 4);
-		for (int c = 0; c <= --(TT->r_size); ++c)
-			{
-				//get the size of the invisible cube
-				int box_size = (TT->r_size) / (2 * pow(2, c)); //8
-				//input 0 or 1
-				int tick = 0;
-				//amount of switches
-				int ticksum = (TT->r_size) / box_size; //2
-				//count input rows
-				int count = 1;
-				//sum of flips of 0 and 1	
-				int flip = 0;
 
-				if (flip < ticksum)
-					{
-						for (int r = 0; c <= --(TT->c_size); ++r)
-							{
-								if (count < box_size)
-									{
-										if (tick == 0)
-											bm_write(TT, r, c, 0);
-										else if (tick == 1)
-											bm_write(TT, r, c, 1);
+	
+	BM* table = bm_make(32, 5);
+	//bm_print(table);
+	
+	for (int c = 1; c <= (table->c_size); ++c)
+	{
+		//get the size of the length of same input
+		double padding_nums = (table->r_size) / (2 * pow(2, c-1)); //8
 
-										++count;
-									}
-								else if (count == box_size)
-									{
-										++flip;
-										tick = !tick;
-									}
-							}
-					}
-			}
+		int row=1;
+		int sum_row=0;
+		double num_rotate =(2 * pow(2, c-1)); // 2
+		//8 rows
+		//4 + 4=8
+		//2+2+2+2=8
+		//1+1+1+1+1+1+1+1=8
+		float sum_rotate=1;
+		while(sum_rotate<=num_rotate){
+		for(;sum_row<padding_nums;++sum_row)
+		{
+			bm_write(table, row, c, 0);
+			++row;
+			
+		}
+			sum_row=0;
+		for(;sum_row<padding_nums;++sum_row)
+		{
+			bm_write(table, row, c, 1);
+			++row;
+		}
+			++sum_rotate;
+			sum_row=0;
+		}
+		
+	}
 
-		bm_print(TT);
+	bm_print(table);
+	bm_destroy(table);
 }
+
+
+//{
+//		//get the size of the invisible cube
+//		int box_size = (table->r_size) / (2 * pow(2, c-1)); //8
+//		//input 0 or 1
+//		int tick = 0;
+//		//amount of switches
+//		int ticksum = (table->r_size) / box_size; //2
+//		//count input rows
+//		int count = 1;
+//		//sum of flips of 0 and 1	
+//		int flip = 0;
+//
+//		if (flip < ticksum)
+//		{
+//			for (int r = 1; r <= (table->r_size); ++r)
+//			{
+//				if (count <= box_size)
+//				{
+//					if (tick == 0)
+//						bm_write(table, r, c, 1);
+//					else if (tick == 1)
+//						bm_write(table, r, c, 1);
+//
+//					++count;
+//				}
+//				else if (count == box_size)
+//				{
+//					++flip;
+//					tick = !tick;
+//				}
+//			}
+//		}
+//	}
